683189b14ad544ba715450822b0d46fee862fb13
gabegm <gabriel@gaucimaistre.com>
2023-03-28 09:51:15 +0200

    initial commit

---
+++ src/main/java/com/gaucimaistre/rssthenews/RssTheNewsApplication.java
@@ -0,0 +1,13 @@
+package com.gaucimaistre.rssthenews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssTheNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssTheNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaucimaistre/rssthenews/RssTheNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaucimaistre.rssthenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssTheNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
